cfb45fcb6dec105fdedfd55cd2b8fd41db60b3df
Rodrigo Oliveira <rodrigo@dienekes.com.br>
2019-05-02 13:35:15 -0300

    final

--- js/toggleState.js
+++ js/toggleState.js
@@ -1,10 +1,16 @@
 function alterContrat(e) {
     e.preventDefault();
-    //alterBackgroundColor();
-    //alterFontColor();
+    mapA();
+    alterBackgroundColor();
+    alterFontColor();
     alterBackgroundColor2();
     alterBackgroundColor3();
     alterBackgroundColor4();
+    alterBackgroundColor5();
+    alterBackgroundColor6();
+    alterBackgroundColor7();
+    alterBackgroundColor8()
+    changeOutlineAll();
 }
 
 function alterBackgroundColor() {
@@ -13,9 +19,10 @@ function alterBackgroundColor() {
     
     if (color !== 'black') {
         contrasteButton.style.backgroundColor = 'black';
-        
+        contrasteButton.style.borderBottom = '2px solid green';
     } else {
         contrasteButton.style.backgroundColor = 'white';
+        contrasteButton.style.borderBottom = '';
     }
 }
 
@@ -23,10 +30,10 @@ function alterFontColor() {
     const font = document.querySelectorAll('nav ul li a');
     let elements = [...font];
     elements.forEach(e => {
-        if (e !== 'white') {
-            e.style.color = 'white'
+        if (e.style.color !== 'white') {
+            e.style.color = 'white';
         } else {
-            e.style.color = 'rgba(0, 0, 0, 0.5)'
+            e.style.color = 'rgba(0, 0, 0, 0.5)';
         }
     });
 }
@@ -64,4 +71,60 @@ function alterBackgroundColor4() {
     } else {
         contrasteButton.style.backgroundColor = '#44C662';
     }
+}
+
+function alterBackgroundColor5() {
+    const div = document.querySelector('#fh5co-features');
+    if (div.style.backgroundColor !== 'black') {
+        div.style.backgroundColor = 'black'
+    } else {
+        div.style.backgroundColor = '';
+    }
+}
+
+
+function alterBackgroundColor6() {
+    const div = document.querySelectorAll('.fh5co-section-gray');
+    Array.from(div).forEach(e => {
+        if (e.style.backgroundColor !== 'black') {
+            e.style.backgroundColor = 'black'
+            e.style.border = '2px solid yellow';
+        } else {
+            e.style.backgroundColor = 'rgba(0, 0, 0, 0.04)';
+            e.style.border = '';
+        }
+    })
+}
+
+function alterBackgroundColor7() {
+    const blockquote = document.querySelectorAll('blockquote');
+    Array.from(blockquote).forEach(e => {
+        if (e.style.backgroundColor !== 'black') {
+            e.style.backgroundColor = 'black'
+        } else {
+            e.style.backgroundColor = 'rgba(0, 0, 0, 0.04)';
+        }
+    })
+}
+
+function changeOutlineAll() {
+    const outlineAll = document.querySelectorAll('.lista');
+    let elements = [...outlineAll];
+    elements.forEach(e => {
+        if (e.style.outlineColor !== 'yellow') {
+            e.style.outlineColor = 'yellow';
+        } else {
+            e.style.outlineColor = 'black';
+        }
+    });
+}
+
+function mapA() {
+    const a = document.querySelectorAll('a');
+    Array.from(a).forEach( e => e.classList.add('lista'))
+}
+
+function alterBackgroundColor8() {
+    const div = document.querySelector('.fh5co-contact');
+    div.style.backgroundColor !== 'black' ?  div.style.backgroundColor = 'black' :  div.style.backgroundColor = '';
 }
